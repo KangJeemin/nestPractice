96f90878839b720ef3d68f5fcf364a5382330340
jiminKang <wlals1234560@gmail.com>
2024-02-16 23:57:43 +0900

    board 정보를 가지고 오는 핸들러 로직 생성

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -3,4 +3,12 @@ import { Injectable } from '@nestjs/common';
 
 //injectable 데코레이터는 다른 컨포넌트에서 이 서비스를 사용할 수 있게 만들어준다.
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+    // 다른 클래스에서 board의 접근을 막음
+    private boards=[];
+
+    getAllBoards() {
+        return this.boards;
+    }
+}
+ 
