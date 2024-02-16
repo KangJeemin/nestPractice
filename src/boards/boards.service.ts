7eec8ec45003ccc744cfd906fbaaf6d47c766b0f
jiminKang <wlals1234560@gmail.com>
2024-02-17 00:18:03 +0900

    유니크한 id값을 부여하기위해 uuid 저굥ㅇ

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './boards.model';
-
+import { Board, BoardStatus } from './boards.model';
+import { v1 as uuid } from 'uuid'
 
 //injectable 데코레이터는 다른 컨포넌트에서 이 서비스를 사용할 수 있게 만들어준다.
 @Injectable()
@@ -10,6 +10,15 @@ export class BoardsService {
 
     getAllBoards():Board[] {
         return this.boards;
+    }
+
+    createBoard(title:string,description:string) {
+        const board:Board = {
+            id:uuid(),
+            title,
+            description,
+            status:BoardStatus.PUBLIC
+        }
     }
 }
  
