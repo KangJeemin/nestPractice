e7b5b25237f7af519465505b91c4d8b5a8047b48
jiminKang <wlals1234560@gmail.com>
2024-02-17 00:10:01 +0900

    board의 타입 적용

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './boards.model';
 
 @Controller('boards')
 export class BoardsController {
     // 의존성 주입
     constructor(private boardsService:BoardsService){}
 
-    @Get()
-    getAllBoard(){
+    @Get('/')
+    getAllBoard():Board[]{
         return this.boardsService.getAllBoards();
     }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './boards.model';
 
 
 //injectable 데코레이터는 다른 컨포넌트에서 이 서비스를 사용할 수 있게 만들어준다.
 @Injectable()
 export class BoardsService {
     // 다른 클래스에서 board의 접근을 막음
-    private boards=[];
+    private boards:Board[]=[];
 
-    getAllBoards() {
+    getAllBoards():Board[] {
         return this.boards;
     }
 }
