01f150c0b94454eed2b554c77e4e74138d7689da
jiminKang <wlals1234560@gmail.com>
2024-02-20 19:13:00 +0900

    기존 로컬 메모리를 사용해 데이터를 가져 오던 방식을 pg에 연결하여 Repository를 이용하여 특정 게시물 가져오는 기능 구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -3,11 +3,13 @@ import { BoardsService } from './boards.service';
 import { BoardStatus } from './board-status.enum';
 import { CreateBoardDto } from './\bdto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
+import { Board } from './board.entity';
 
 @Controller('boards')
 export class BoardsController {
     // 의존성 주입
-    // constructor(private boardsService:BoardsService){}
+    constructor(private boardsService:BoardsService){}
+
 
     // @Get('/')
     // getAllBoard():Board[]{
@@ -22,6 +24,10 @@ export class BoardsController {
     //     return this.boardsService.createBoard(CreateBoardDto)
     // }
 
+    @Get('/:id')
+    getBoardById(@Param('id') id:number) : Promise<Board> {
+        return this.boardsService.getBoardById(id)
+    }
     // @Get('/:id')
     // getBoardById(@Param('id') id:string):Board{ 
     //     return this.boardsService.getBoardById(id)
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -2,10 +2,20 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { BoardStatus } from './board-status.enum';
 import { v1 as uuid } from 'uuid'
 import { CreateBoardDto } from './\bdto/create-board.dto';
+import { BoardRepository } from './board.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './board.entity';
 
 //injectable 데코레이터는 다른 컨포넌트에서 이 서비스를 사용할 수 있게 만들어준다.
 @Injectable()
 export class BoardsService {
+    constructor(
+        // 이 데코레이터를 이용해서 이 서비스에서 BoardRepository를 이용한다고 이걸 boardRepository 변수에 넣어줍니다.
+        @InjectRepository(BoardRepository)
+        private boardRepository: BoardRepository,
+    ){
+
+    }
     // 다른 클래스에서 board의 접근을 막음
     // private boards:Board[]=[];
 
@@ -25,6 +35,13 @@ export class BoardsService {
     //     return board;
     // }
     
+    async getBoardById(id: number):Promise <Board> {
+        const found = await this.boardRepository.findOne(id); 
+        if(!found){
+            throw new NotFoundException(`Cant Board with id ${id}`)
+        }
+        return found;
+    }
     // getBoardById(id:string):Board {
     //     const found= this.boards.find((board)=> board.id ===id)
 
