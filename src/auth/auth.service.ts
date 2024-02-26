20e6962cd3b563b1895ac584520eb0ce70f5db9e
jiminKang <wlals1234560@gmail.com>
2024-02-26 14:57:07 +0900

    auth service 파일에서 User Repository 파일 사용하기

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
 @Injectable()
@@ -8,7 +9,9 @@ export class AuthService {
         @InjectRepository(User)
         private userRepository:UserRepository,
     ){
-
+        
     }
-
+    async signUp(authCredentialsDto:AuthCredentialsDto):Promise<void> {
+        return this.userRepository.createUser(authCredentialsDto)
+    }
 }
