f93ba36f61e488662236db77c8290a5b7226c9af
jiminKang <wlals1234560@gmail.com>
2024-02-26 14:54:41 +0900

    User Repository 파일 작성

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,12 @@
-import { EntityRepository, Repository } from "typeorm";
+import { Repository } from "typeorm";
+import { AuthCredentialsDto } from "./dto/auth-credential.dto";
 import { User } from './user.entity'
 
-@EntityRepository(User)
-export class BoardRepository extends Repository<User> {
 
+export class UserRepository extends Repository<User> {
+    async createUser(authCredentialsDto:AuthCredentialsDto):Promise<void>{
+        const { username, password}=authCredentialsDto
+        const user = this.create({username,password});
+        await this.save(user);
+    }  
 }
