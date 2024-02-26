2322694315475e862e185d11286602fb90355344
jiminKang <wlals1234560@gmail.com>
2024-02-26 16:53:38 +0900

    User repository 파일을 바꿔 봤는데도 server 500 에러 발생하는 중

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,19 +1,21 @@
-import { Repository } from "typeorm";
+import { getRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
 import { User } from './user.entity'
 
+export class UserRepository {
+    private userRepository: Repository<User>;
 
-export class UserRepository extends Repository<User> {
-    
-    async createUser(authCredentialsDto:AuthCredentialsDto):Promise<void>{
-        try{
-            const { username, password }=authCredentialsDto
-            const user = this.create({ username,password });
-            await this.save(user);
+    constructor() {
+        this.userRepository = getRepository(User);
+    }
+
+    async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        try {
+            const { username, password } = authCredentialsDto;
+            const user = this.userRepository.create({ username, password });
+            await this.userRepository.save(user);
+        } catch (error) {
+            throw new Error(`Failed to create user: ${error.message}`);
         }
-        catch (error) {
-            throw new Error(`Failed to create board: ${error.message}`);
-        }
-        
-    }  
+    }
 }
