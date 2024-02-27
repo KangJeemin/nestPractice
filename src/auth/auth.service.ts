03685bf4d4963704e7706f87e1cc608aefe7d815
jiminKang <wlals1234560@gmail.com>
2024-02-27 18:22:25 +0900

    로그인 기능 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
@@ -14,24 +14,33 @@ export class AuthService {
     ){}
     async signUp(authCredentialsDto:AuthCredentialsDto):Promise<void> {
             
-                const { username, password }= authCredentialsDto
-                const salt = await bcrypt.genSalt();
-                const hashedPassword = await bcrypt.hash(password,salt)
+        const { username, password }= authCredentialsDto
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password,salt)
 
-                const user = this.userRepository.create({ username,password:hashedPassword });
-                try{
-                    await this.userRepository.save(user);
-                } catch(error) {
-                    if(error.code === '23505') {
-                        throw new ConflictException('Existing username')
-                    }
-                    else{
-                        throw new InternalServerErrorException();
-                    }
-
-                    console.log(error)
-                }
-                
+        const user = this.userRepository.create({ username,password:hashedPassword });
+        try{
+            await this.userRepository.save(user);
+        } catch(error) {
+            if(error.code === '23505') {
+                throw new ConflictException('Existing username')
             }
+            else{
+                throw new InternalServerErrorException();
+            }
+        }
+                
+        }
+    async signIn(authCredentialsDto:AuthCredentialsDto):Promise<string> {
+        const { username, password}= authCredentialsDto
+        const user = await this.userRepository.findOne({where:{username}});
+
+        if(user && (await bcrypt.compare(password,user.password))){
+            return 'login Success'
+        }
+        else{
+            throw new UnauthorizedException('login failed');
+        }
+    }
             
         } 
--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,9 +1,9 @@
 import { BaseEntity,Column,Entity,PrimaryGeneratedColumn,Unique } from "typeorm";
 
 // entity의 클래스 이름이 테이블 이름으로 설정됨.
-@Entity()
-
 //unique 지정(username에서 중복 된 값이 발생할 경우 데이터베이스 레벨에서 에러 처리.)
+
+@Entity()
 @Unique(['username'])
 export class User extends BaseEntity {
     @PrimaryGeneratedColumn()
