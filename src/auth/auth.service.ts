e947db455bf06bc83729465fb20e35b62e7a58bf
jiminKang <wlals1234560@gmail.com>
2024-02-27 18:53:23 +0900

    로그인시 토큰 생성하는 기능 완료

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,12 +5,15 @@ import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
 import * as bcrypt from 'bcryptjs'
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(User)
         private userRepository:UserRepository,
+        // auth 모듈에 JWT를 등록해주었기 떄문에 Service에서 JWT를 가져올 수 있다. 
+        private jwtService:JwtService
     ){}
     async signUp(authCredentialsDto:AuthCredentialsDto):Promise<void> {
             
@@ -31,13 +34,19 @@ export class AuthService {
         }
                 
         }
-    async signIn(authCredentialsDto:AuthCredentialsDto):Promise<string> {
+    async signIn(authCredentialsDto:AuthCredentialsDto):Promise<{accessToken:string}> {
         const { username, password}= authCredentialsDto
         const user = await this.userRepository.findOne({where:{username}});
 
         if(user && (await bcrypt.compare(password,user.password))){
-            return 'login Success'
-        }
+            // 유저 토큰 생성 (secret + payload )
+            // payload 에는 중요한 정보를 넣으면 안 됨.( 탈취 가능성 ) 
+            const payload = { username };
+            // secret 과 payload를 합쳐서 토큰을 생성함.
+            const accessToken =await this.jwtService.sign(payload);
+
+            return { accessToken }
+        } 
         else{
             throw new UnauthorizedException('login failed');
         }
