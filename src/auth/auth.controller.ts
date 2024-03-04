c2114a445e131b98bfcb3b9bf3c703905a3be5cb
jiminKang <wlals1234560@gmail.com>
2024-03-04 16:28:00 +0900

    커스텀 데코레이터 만들기

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Get, Post, Req, UseGuards, ValidationPipe } from '@ne
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { AuthGuard} from '@nestjs/passport';
+import { GetUser } from './get-user.decorater';
+import { User } from './user.entity';
 
 
 @Controller('auth')
@@ -18,10 +20,10 @@ export class AuthController {
         return this.authService.signIn(authCredentialsDto)
     }
 
-    @UseGuards(AuthGuard())
     @Post('/test')    
-    test(@Req() req){
-        console.log('req=',req)
+    @UseGuards(AuthGuard())
+    test(@GetUser() user:User){
+        console.log('user=',user)
     }
     // UseGuards 안에 @nestjs/passport에서 가져온 AuthGuard()를 이용하면 요청안에 유저 정보를 넣어줄 수 있습니다.
     // @Post('/authTest')
