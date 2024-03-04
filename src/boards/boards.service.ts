33f404dc2b335400fd03989b9d4e9fa24e997997
jiminKang <wlals1234560@gmail.com>
2024-03-04 16:54:53 +0900

    게시물을 생성할때 자동으로 유저 정보도 기입되게 설정

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -6,6 +6,7 @@ import { BoardRepository } from './board.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Board } from './board.entity';
 import { Repository } from 'typeorm';
+import { User } from 'src/auth/user.entity';
 
 //injectable 데코레이터는 다른 컨포넌트에서 이 서비스를 사용할 수 있게 만들어준다.
 @Injectable()
@@ -28,7 +29,7 @@ export class BoardsService {
         return this.boardRepository.find()
         
     }
-    async createBoard(CreateBoardDto:CreateBoardDto) : Promise<Board>{
+    async createBoard(CreateBoardDto:CreateBoardDto,user:User) : Promise<Board>{
         const {title, description} = CreateBoardDto;
 
         try{
@@ -36,7 +37,9 @@ export class BoardsService {
             const board = this.boardRepository.create({
                 title,
                 description,
-                status:BoardStatus.PUBLIC
+                status:BoardStatus.PUBLIC,
+                user
+
             })
     
             // 데이터베이스에 값 저장
